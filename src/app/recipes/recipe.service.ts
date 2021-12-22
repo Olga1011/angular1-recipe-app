261481c219595a1cd31f2f0d5e6ec4ebc0da3312
Olga1011 <56080331+Olga1011@users.noreply.github.com>
2021-12-22 14:47:52 +0200

    initial commit

---
+++ src/app/recipes/recipe.service.ts
@@ -0,0 +1,52 @@
+import { Injectable } from '@angular/core';
+import { Subject } from 'rxjs';
+import { Recipe } from './recipe.model';
+
+import { Ingredient } from '../shared/ingredient.model';
+import { ShoppingListService } from '../shopping-list/shopping-list.service';
+
+@Injectable()
+export class RecipeService {
+  recipesChanged = new Subject<Recipe[]>();
+  recipes: Recipe[] = [
+    new Recipe(
+      'A test recipe',
+      'This is a simply recipe',
+      'https://images.immediate.co.uk/production/volatile/sites/30/2020/08/chorizo-mozarella-gnocchi-bake-cropped-9ab73a3.jpg?quality=90&resize=556,505',
+      [new Ingredient('meat', 2), new Ingredient('bread', 3)]
+    ),
+
+    new Recipe(
+      'Second test recipe',
+      'This is a simply recipe',
+      'https://images.immediate.co.uk/production/volatile/sites/30/2020/08/chorizo-mozarella-gnocchi-bake-cropped-9ab73a3.jpg?quality=90&resize=556,505',
+      [new Ingredient('meat', 2), new Ingredient('bread', 3)]
+    ),
+  ];
+
+  constructor(private slService: ShoppingListService) {}
+  getRecipes() {
+    return this.recipes.slice();
+  }
+  getRecipe(index: number) {
+    return this.recipes.slice()[index];
+  }
+
+  addIngredientsToShoppingList(ingredients: Ingredient[]) {
+    this.slService.addIngredients(ingredients);
+  }
+  addRecipe(recipe: Recipe) {
+    this.recipes.push(recipe);
+    this.recipesChanged.next(this.recipes.slice());
+  }
+
+  updateRecipe(index: number, newRecipe: Recipe) {
+    this.recipes[index] = newRecipe;
+    this.recipesChanged.next(this.recipes.slice());
+  }
+
+  deleteRecipe(index: number) {
+    this.recipes.splice(index, 1);
+    this.recipesChanged.next(this.recipes.slice());
+  }
+}
